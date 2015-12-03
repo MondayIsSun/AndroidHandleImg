ef50d5a83c447293085d5819222320791c23973c
MondayIsSun <mr.lmd@outlook.com>
2015-12-03 10:51:49 +0800

    create app

---
+++ xysimageop2/src/main/java/mr/lmd/personal/xysimageop2/RoundRectXfermodTest.java
@@ -0,0 +1,13 @@
+package mr.lmd.personal.xysimageop2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoundRectXfermodTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.round_rect_xfermode);
+    }
+}
